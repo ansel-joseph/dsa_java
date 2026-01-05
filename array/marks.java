eaee96ce546dd415795806cac8dfbd4a9d996a11
Ansel Joseph <anseljoseph2509@gmail.com>
2026-01-05 19:27:52 +0530

    add: array_beginning

---
+++ array/marks.java
@@ -0,0 +1,12 @@
+public class marks {
+    public static void main(String[] args) {
+        int[] marks = new int [3];
+        marks[0] = 67;
+        marks[1] = 98;
+        marks[2] = 79;
+        for (int i = 0; i< 3; i++){
+            System.out.println(marks[i]);
+        }
+
+    }
+}
