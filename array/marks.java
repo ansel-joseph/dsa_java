2e85e1d7b831ae586af2c629fa775a064de9fd9f
Ansel Joseph <anseljoseph2509@gmail.com>
2026-01-05 23:38:19 +0530

    add: array declaration

--- array/marks.java
+++ array/marks.java
@@ -1,9 +1,7 @@
 public class marks {
     public static void main(String[] args) {
-        int[] marks = new int [3];
-        marks[0] = 67;
-        marks[1] = 98;
-        marks[2] = 79;
+        int[] marks = {78, 90, 69};
+
         for (int i = 0; i< 3; i++){
             System.out.println(marks[i]);
         }
