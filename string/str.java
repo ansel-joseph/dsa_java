700d93d25476364df3da5430fe4957c5391cb456
Ansel Joseph <anseljoseph2509@gmail.com>
2026-01-11 18:40:32 +0530

    add: string concatenation

--- string/str.java
+++ string/str.java
@@ -1,6 +1,7 @@
 public class str {
     public static void main(String args[]){
-        String str1 = "Hello World";
-        System.out.println(str1);
+        String str1 = "Bruce";
+        String str2 = "Wayne";
+        System.out.println(str1 + " " + str2); //concatenation
     }
 }
