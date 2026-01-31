4b24cda0cae8d699e29241c0b64a94d0eb55959a
Ansel Joseph <anseljoseph2509@gmail.com>
2026-01-31 10:58:01 +0530

    add: input array

--- array/array.java
+++ array/array.java
@@ -1,29 +1,20 @@
-import java.util.*;
+    import java.util.Scanner;
+    public class array{
+        public static void main (String [] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the size of the array: ");
+            int size= sc.nextInt();
 
-public class array {
-    public static void main(String[] args) {
-        int[][] array = new int[3][4];
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of rows and columns:");
-        int rows = sc.nextInt();
-        int cols = sc.nextInt();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.println("Enter element for position (" + i + "," + j + "):");
-                array[i][j] = sc.nextInt();
+            System.out.println("Enter the elements of the array: ");
+            int arr[]= new int[size];
+            for(int i=0; i<size; i++){
+                arr[i] = sc.nextInt();
             }
-        }
-        System.out.println("Enter the element to be found: ");
-        int x = sc.nextInt();
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (array[i][j]==x){
-                    System.out.println("Element found at : " + i + "," + j);
-                }
+            System.out.println("The array elements are: ");
+            for(int i=0; i<size; i++){
+                System.out.println(arr[i]+" ");
             }
-            System.out.println();
-        }
 
+        }
     }
-}
